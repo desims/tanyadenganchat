c29108efbc3c96afca87d64b7a293c4ca533c0f5
Desi Sitompul <sdesimayasari@gmail.com>
2024-10-31 14:42:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ if "messages" not in st.session_state.keys():  # Initialize the chat messages hi
     st.session_state.messages = [
         {
             "role": "assistant",
-            "content": "Ask me a question about Streamlit's open-source Python library!",
+            "content": "Ajukan saya pertanyaan mengenai pedoman penulisan tugas akhir!",
         }
     ]
 
